cb18e5a300223bce955cc7dff5145f06afd82dff
heyitsmeravi <ravijnv2428@gmail.com>
2026-07-18 11:13:14 +0530

    User updation setup and validation setup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,9 @@
 const User = require("../models/User");
+const mongoose = require("mongoose");
 const getAllUsers = async (req, res) =>{
     try{
         const users = await User.find().select("-password");
-        console.log(req);
+        // console.log(req);
         return res.status(200).json({
             message:"Data fetched successfully",
             users
@@ -32,6 +33,84 @@ const getMyProfile = async (req, res) => {
         });
     }
 }
+const getUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                message:"Invalid user ID"
+            });
+        }
+        const user = await User.findById(id).select("-password");
+        if (user == null){
+            return res.status(404).json({
+                message:"User not found"
+            });   
+        }
+        return res.status(200).json({
+            message:"Data fetched successfully",
+            user
+        });
+        
+    } catch(err){
+        return res.status(500).json({
+            message:"something went wrong"
+        });
+    }
+}
+const ALLOWED_PROFILE_FIELDS = [
+            "name"
+        ];
+const updateMyProfile = async (req , res) =>{
+    try {
+        const requestedFields = Object.keys(req.body);
+        if (requestedFields.length === 0 ){
+            return res.status(400).json({
+                message:"No fields provided for update"
+            });
+        }
+        for (const field of requestedFields){
+            if (!ALLOWED_PROFILE_FIELDS.includes(field)){
+                return res.status(400).json({
+                    message:`${field} can't be updated`
+                });
+            }
+            if (field === "name"){
+                if (req.body[field].trim() === ""){
+                    return res.status(400).json({
+                        message:"Name cannot be empty."
+                    });
+                }
+            }
+        }
+        const changes = await User.findByIdAndUpdate(req.user.id,req.body,{
+            returnDocument:"after",
+            runValidators:true
+        });
+        if (changes == null){
+            return res.status(404).json({
+                message:"User not found"
+            });
+        }
+        const { password, ...userWithoutPassword} = changes.toObject();
+        // console.log(user);
+        return res.status(200).json({
+            message:"data updated successfully",
+            user:userWithoutPassword
+        })
+        // console.log("PATCH route hit");
+        // console.log("when in update profile",req.body);
+    } catch (err){
+        if (err.name === "ValidationError"){
+            return res.status(400).json({
+                message: err.message
+            })
+        }
+        return res.status(500).json({
+            message:"something went wrong"
+        });
+    }
+}
 module.exports = {
-    getAllUsers , getMyProfile
+    getAllUsers , getMyProfile , getUser , updateMyProfile
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 //controllers
-const {getAllUsers,getMyProfile} = require("../controllers/userController");
+const {getAllUsers,getMyProfile,getUser, updateMyProfile} = require("../controllers/userController");
 //middleware
 const authMiddleware= require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 router.get("/",authMiddleware,roleMiddleware(["admin"]),getAllUsers);
 router.get("/profile",authMiddleware,roleMiddleware(["student","admin","faculty"]),getMyProfile);
+router.get("/:id",authMiddleware,roleMiddleware(["admin"]),getUser);
+router.patch("/profile",authMiddleware, roleMiddleware(["student","admin","faculty"]),updateMyProfile);
 module.exports = router;
